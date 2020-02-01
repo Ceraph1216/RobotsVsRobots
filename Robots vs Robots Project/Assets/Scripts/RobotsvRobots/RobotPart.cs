bdc3761023c6b47a11b56001e0a355308f6ae135
ceraph1216 <maverickhunterdn@gmail.com>
2020-02-01 00:26:58 -0500

    - added ability to click and match top and bottom robot parts - added test scene with test robot parts

---
+++ Robots vs Robots Project/Assets/Scripts/RobotsvRobots/RobotPart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotPart : MonoBehaviour
+{
+    public PartType partType;
+
+    private Image _selectionHighlight;
+
+    void Awake ()
+    {
+        _selectionHighlight = GetComponent<Image>();
+    }
+
+    public void OnClick ()
+    {
+       RobotProductionManager.instance.SelectPart(this);
+    }
+
+    public void Select ()
+    {
+        _selectionHighlight.enabled = true;
+    }
+
+    public void Deselect ()
+    {
+        _selectionHighlight.enabled = false;
+    }
+
+    public void OnMatch ()
+    {
+        Deselect();
+        gameObject.SetActive(false); // TODO: use object pooling to unspawn
+    }
+}
+
+public enum PartType
+{
+    Top,
+    Bottom
+}
