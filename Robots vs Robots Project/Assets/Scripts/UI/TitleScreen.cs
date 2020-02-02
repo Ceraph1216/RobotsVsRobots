49a3efac1e576538878c810ca8054fad3187389d
Bryan <bryan@brinkbit.com>
2020-02-01 20:29:21 -0500

    Add title screen and player icons

--- Robots vs Robots Project/Assets/Scripts/UI/TitleScreen.cs
+++ Robots vs Robots Project/Assets/Scripts/UI/TitleScreen.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TitleScreen : MonoBehaviour 
+public class TitleScreen : MonoBehaviour
 {
 	public GameObject titleScreen;
 
 	public void OnClickPlay ()
 	{
-		if (SoftPauseScript.softPaused) 
+		if (SoftPauseScript.softPaused)
 		{
 			return;
 		}
@@ -18,6 +18,6 @@ public class TitleScreen : MonoBehaviour
 
 	public void LoadGame ()
 	{
-		UnityEngine.SceneManagement.SceneManager.LoadScene ("TestScene");
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("GameScreen");
 	}
 }
