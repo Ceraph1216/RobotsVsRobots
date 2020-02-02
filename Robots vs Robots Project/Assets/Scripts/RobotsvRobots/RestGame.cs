518f64349041c86aae916cd9d0611817f2e16a7f
Ceraph1216 <MaverickHunterDN@gmail.com>
2020-02-02 15:19:06 -0500

    - added button to reset game when a player winas

---
+++ Robots vs Robots Project/Assets/Scripts/RobotsvRobots/RestGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestGame : MonoBehaviour
+{
+    public void OnClickRestart ()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScreen");
+    }
+}
