0f77ce8572f1c2092ce8c43318c4fdc3462b9c24
Richard Van Tassel <rickvt@gmail.com>
2020-02-02 00:19:27 -0500

    Adds random number generator, x position fields in production manager inspector

--- Robots vs Robots Project/Assets/Scripts/RobotsvRobots/RobotProductionManager.cs
+++ Robots vs Robots Project/Assets/Scripts/RobotsvRobots/RobotProductionManager.cs
@@ -17,12 +17,21 @@ public class RobotProductionManager : MonoBehaviour
 
     Dictionary<Robot.RobotTeam, Robot> curRobots;
 
+    Dictionary<Robot.RobotTeam, List<int>> recentPRNGResults;
+
+    [SerializeField] float leftSpawnX;
+    [SerializeField] float rightSpawnX;
+
     void Awake ()
     {
         instance = this;
 
         selectedMovement = new Dictionary<Robot.RobotTeam, RobotMovement>();
         selectedDamager = new Dictionary<Robot.RobotTeam, RobotDamager>();
+
+        recentPRNGResults = new Dictionary<Robot.RobotTeam, List<int>>();
+        recentPRNGResults.Add(Robot.RobotTeam.Left, new List<int>());
+        recentPRNGResults.Add(Robot.RobotTeam.Right, new List<int>());
 
         cursorPositions = new Dictionary<Robot.RobotTeam, int>();
         cursorPositions.Add(Robot.RobotTeam.Left, 0);
@@ -119,7 +128,7 @@ public class RobotProductionManager : MonoBehaviour
 
     Vector2 GetLaneStartPosition(Robot.RobotTeam team)
     {
-        return (team == Robot.RobotTeam.Left ? Vector2.left : Vector2.right) * 5 + lanes[cursorPositions[team]].transform.position.y * Vector2.up;
+        return new Vector2(team == Robot.RobotTeam.Left ? leftSpawnX : rightSpawnX, lanes[cursorPositions[team]].transform.position.y);
     }
 
     private void ReleaseRobot(Robot.RobotTeam team)
@@ -132,8 +141,58 @@ public class RobotProductionManager : MonoBehaviour
         Debug.Log("Set player to part selection: " + team);
     }
 
+    public float GetPRNGPartNumber(Robot.RobotTeam team)
+    {
+        int range = 4;
+        int recentCount = 8;
+
+        List<int> recents = recentPRNGResults[team];
+
+        int result;
+
+        if (recents.Count == recentCount)
+        {
+            List<int> candidates = new List<int>();
+            for (int i = 0; i < range; i++)
+            {
+                if (!recents.Contains(i))
+                {
+                    candidates.Add(i);
+                }
+            }
+
+            if (candidates.Count > 0)
+            {
+                result = candidates[Random.Range(0, candidates.Count)];
+            }
+            else
+            {
+                result = Random.Range(0, range);
+            }
+     
+        }
+        else
+        {
+            result = Random.Range(0, range);
+        }
+
+        recents.Add(result);
+        if (recents.Count > recentCount) recents.RemoveAt(0);
+        return result;
+    }
+
     private void OnGUI()
     {
+        GUI.Box(new Rect(180, 10, 140, 90), "PRNG testing");
+        if (GUI.Button(new Rect(190, 40, 120, 20), "Get sample Left"))
+        {
+            Debug.Log("PRNG result: " + GetPRNGPartNumber(Robot.RobotTeam.Left));
+        }
+        if (GUI.Button(new Rect(190, 70, 120, 20), "Get sample Right"))
+        {
+            Debug.Log("PRNG result: " + GetPRNGPartNumber(Robot.RobotTeam.Right));
+        }
+
         GUI.Box(new Rect(330, 10, 140, 90), "Choose Left's Lane");
 
         if (GUI.Button(new Rect(340, 40, 120, 20), "lane up"))
